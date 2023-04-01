6deecd8e5796a42af91b29e37d747cb549c959e8
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-04-01 03:54:48 -0300

    utilizando biblioteca math

--- exercicio7.py
+++ exercicio7.py
@@ -1,14 +1,11 @@
 #  Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+import math
 
 lis = []
 for x in range(5):
     numero = int(input(f"Insira o {x + 1}º número: "))
     lis.append(numero)
 
-produto = 1
-for x in range(5):
-    produto *= lis[x]
-
 print(lis)
 print("Soma entre os números:", sum(lis))
-print("Produto dos números:", produto)
+print("Produto dos números:", math.prod(lis))
