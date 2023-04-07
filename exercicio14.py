3ed470750998fa1efcd9f3616c2dd227adcc7c79
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-04-07 00:09:53 -0300

    mudança na formatação da lista

--- exercicio14.py
+++ exercicio14.py
@@ -9,9 +9,12 @@
 # no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita",
 # entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 
-lista_perguntas = ["Telefonou para a vítima? ", "Esteve no local do crime? ", "Mora perto da vítima? ",
+lista_perguntas = ["Telefonou para a vítima? ",
+                   "Esteve no local do crime? ",
+                   "Mora perto da vítima? ",
                    "Devia para a vítima? ",
                    "Já trabalhou com a vítima? "]
+
 lista_condicoes = ["Suspeita", "Cúmplice", "Assassino", "Inocente"]
 
 respostas = 0
